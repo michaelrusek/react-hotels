a73e7cd0deeedb7a27def610d08c5787fd648e00
michaelrusek <de.mrusso@gmail.com>
2023-06-14 09:03:10 +0200

    fix: room component

--- src/components/Room/Room.tsx
+++ src/components/Room/Room.tsx
@@ -13,23 +13,18 @@ const Room = ({
   const roomOccupancy: RoomOccupancy = room.occupancy;
 
   return (
-    <>
-      {adults >= roomOccupancy.maxAdults &&
-        children <= roomOccupancy.maxChildren && (
-          <article className="room">
-            <div className="room__details">
-              <h3 className="room__title">{room.name}</h3>
-              <p className="room__guests">
-                <strong>Adults:</strong> {roomOccupancy.maxAdults}
-              </p>
-              <p className="room__guests">
-                <strong>Children:</strong> {roomOccupancy.maxChildren}
-              </p>
-            </div>
-            <p className="room__description">{room.longDescription}</p>
-          </article>
-        )}
-    </>
+    <article className="room">
+      <div className="room__details">
+        <h3 className="room__title">{room.name}</h3>
+        <p className="room__guests">
+          <strong>Adults:</strong> {roomOccupancy.maxAdults}
+        </p>
+        <p className="room__guests">
+          <strong>Children:</strong> {roomOccupancy.maxChildren}
+        </p>
+      </div>
+      <p className="room__description">{room.longDescription}</p>
+    </article>
   );
 };
 
